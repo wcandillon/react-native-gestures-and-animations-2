6155b6d8a821ca4bc5074f7c0a8af61dfbb40658
William Candillon <wcandillon@gmail.com>
2020-07-31 11:34:32 +0200

    :wrench:

--- src/DynamicSpring/DynamicSpring.tsx
+++ src/DynamicSpring/DynamicSpring.tsx
@@ -15,6 +15,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  card: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+  },
 });
 
 interface DynamicSpringProps {
@@ -25,26 +32,15 @@ interface DynamicSpringProps {
 const DynamicSpring = ({ width, height }: DynamicSpringProps) => {
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
-
   const t2X = useDerivedValue(() => withSpring(translateX.value));
   const t2Y = useDerivedValue(() => withSpring(translateY.value));
   const style2 = useAnimatedStyle(() => {
     return {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: CARD_WIDTH,
-      height: CARD_HEIGHT,
       transform: [{ translateX: t2X.value }, { translateY: t2Y.value }],
     };
   });
   const style3 = useAnimatedStyle(() => {
     return {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: CARD_WIDTH,
-      height: CARD_HEIGHT,
       transform: [
         { translateX: withSpring(t2X.value) },
         { translateY: withSpring(t2Y.value) },
@@ -53,10 +49,10 @@ const DynamicSpring = ({ width, height }: DynamicSpringProps) => {
   });
   return (
     <View style={styles.container}>
-      <Animated.View style={style3}>
+      <Animated.View style={[styles.card, style3]}>
         <Card card={Cards.Card3} />
       </Animated.View>
-      <Animated.View style={style2}>
+      <Animated.View style={[styles.card, style2]}>
         <Card card={Cards.Card2} />
       </Animated.View>
       <DraggableCard {...{ translateX, translateY, width, height }} />

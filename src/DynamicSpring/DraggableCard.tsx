eecfb499740ae3a06e8d65f156764b4544a67812
William Candillon <wcandillon@gmail.com>
2020-07-31 11:32:29 +0200

    :wrench:

---
+++ src/DynamicSpring/DraggableCard.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+
+import { Card, Cards, CARD_WIDTH, CARD_HEIGHT } from "../components";
+import { withDecay, clamp } from "../components/AnimatedHelpers";
+
+interface DraggableCardProps {
+  translateX: Animated.SharedValue<number>;
+  translateY: Animated.SharedValue<number>;
+  width: number;
+  height: number;
+}
+
+const DraggableCard = ({
+  translateX,
+  translateY,
+  width,
+  height,
+}: DraggableCardProps) => {
+  const boundX = width - CARD_WIDTH;
+  const boundY = height - CARD_HEIGHT;
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX);
+      translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY);
+    },
+    onEnd: ({ velocityX, velocityY }) => {
+      translateX.value = withDecay({
+        velocity: velocityX,
+        clamp: [0, boundX],
+      });
+      translateY.value = withDecay({
+        velocity: velocityY,
+        clamp: [0, boundY],
+      });
+    },
+  });
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+  return (
+    <PanGestureHandler {...{ onGestureEvent }}>
+      <Animated.View {...{ style }}>
+        <Card card={Cards.Card1} />
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
+
+export default DraggableCard;
--- src/DynamicSpring/DynamicSpring.tsx
+++ src/DynamicSpring/DynamicSpring.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import Animated, {
-  useAnimatedGestureHandler,
   useSharedValue,
   useAnimatedStyle,
   withSpring,
   useDerivedValue,
 } from "react-native-reanimated";
-import { PanGestureHandler } from "react-native-gesture-handler";
 
 import { Card, Cards, CARD_WIDTH, CARD_HEIGHT } from "../components";
-import { withDecay, clamp } from "../components/AnimatedHelpers";
+
+import DraggableCard from "./DraggableCard";
 
 const styles = StyleSheet.create({
   container: {
@@ -24,38 +23,9 @@ interface DynamicSpringProps {
 }
 
 const DynamicSpring = ({ width, height }: DynamicSpringProps) => {
-  const boundX = width - CARD_WIDTH;
-  const boundY = height - CARD_HEIGHT;
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
-  const onGestureEvent = useAnimatedGestureHandler({
-    onStart: (_, ctx) => {
-      ctx.offsetX = translateX.value;
-      ctx.offsetY = translateY.value;
-    },
-    onActive: (event, ctx) => {
-      translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX);
-      translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY);
-    },
-    onEnd: ({ velocityX, velocityY }) => {
-      translateX.value = withDecay({
-        velocity: velocityX,
-        clamp: [0, boundX],
-      });
-      translateY.value = withDecay({
-        velocity: velocityY,
-        clamp: [0, boundY],
-      });
-    },
-  });
-  const style = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: translateX.value },
-        { translateY: translateY.value },
-      ],
-    };
-  });
+
   const t2X = useDerivedValue(() => withSpring(translateX.value));
   const t2Y = useDerivedValue(() => withSpring(translateY.value));
   const style2 = useAnimatedStyle(() => {
@@ -83,19 +53,13 @@ const DynamicSpring = ({ width, height }: DynamicSpringProps) => {
   });
   return (
     <View style={styles.container}>
-      <PanGestureHandler {...{ onGestureEvent }}>
-        <Animated.View style={StyleSheet.absoluteFill}>
-          <Animated.View style={style3}>
-            <Card card={Cards.Card3} />
-          </Animated.View>
-          <Animated.View style={style2}>
-            <Card card={Cards.Card2} />
-          </Animated.View>
-          <Animated.View {...{ style }}>
-            <Card card={Cards.Card1} />
-          </Animated.View>
-        </Animated.View>
-      </PanGestureHandler>
+      <Animated.View style={style3}>
+        <Card card={Cards.Card3} />
+      </Animated.View>
+      <Animated.View style={style2}>
+        <Card card={Cards.Card2} />
+      </Animated.View>
+      <DraggableCard {...{ translateX, translateY, width, height }} />
     </View>
   );
 };

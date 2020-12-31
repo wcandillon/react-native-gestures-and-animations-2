fd65336367fdefea683784bb3f44dd06dd324bc8
William Candillon <wcandillon@gmail.com>
2020-12-31 14:13:49 +0100

    :wrench:

--- src/Swiping/boilerplate/Swiping.tsx
+++ src/Swiping/boilerplate/Swiping.tsx
@@ -8,25 +8,25 @@ export const profiles: ProfileModel[] = [
     id: "1",
     name: "Caroline",
     age: 24,
-    profile: require("./assets/1.jpg"),
+    profile: require("../assets/1.jpg"),
   },
   {
     id: "2",
     name: "Jack",
     age: 30,
-    profile: require("./assets/2.jpg"),
+    profile: require("../assets/2.jpg"),
   },
   {
     id: "3",
     name: "Anet",
     age: 21,
-    profile: require("./assets/3.jpg"),
+    profile: require("../assets/3.jpg"),
   },
   {
     id: "4",
     name: "John",
     age: 28,
-    profile: require("./assets/4.jpg"),
+    profile: require("../assets/4.jpg"),
   },
 ];
 

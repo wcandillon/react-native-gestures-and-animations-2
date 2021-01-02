61b5427bbfd7896a6dc6b60dd36a61e3762f7a93
William Candillon <wcandillon@gmail.com>
2021-01-02 15:01:05 +0100

    :wrench:

--- src/ShapeMorphing/boilerplate/Eye.tsx
+++ src/ShapeMorphing/boilerplate/Eye.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import Svg, { Path } from "react-native-svg";
-import {
-  addCurve,
-  close,
-  createPath,
-  interpolatePath,
-  serialize,
-} from "react-native-redash";
+import { addCurve, close, createPath, serialize } from "react-native-redash";
 
 interface EyeProps {
   progress: number;

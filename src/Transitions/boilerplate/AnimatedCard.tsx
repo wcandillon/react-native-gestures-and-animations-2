87af238d50f5174749de0c01a0f892b55f8b541a
MD. MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2021-12-14 19:31:19 +0600

    implement extra stuff on transitions

--- src/Transitions/boilerplate/AnimatedCard.tsx
+++ src/Transitions/boilerplate/AnimatedCard.tsx
@@ -1,6 +1,17 @@
+interface AnimatedCardProps {
+  transition: any;
+  index: number;
+  card: Cards;
+}
+
 import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  interpolate,
+  interpolateNode,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { mix } from "react-native-redash";
 
 import { Card, Cards, StyleGuide } from "../../components";
 
@@ -15,21 +26,21 @@ const styles = StyleSheet.create({
   },
 });
 
-interface AnimatedCardProps {
-  toggled: boolean;
-  index: number;
-  card: Cards;
-}
-
-const AnimatedCard = ({ card, toggled, index }: AnimatedCardProps) => {
-  const alpha = toggled ? ((index - 1) * Math.PI) / 6 : 0;
-  const style = {
-    transform: [
-      { translateX: origin },
-      { rotate: `${alpha}rad` },
-      { translateX: -origin },
-    ],
-  };
+const AnimatedCard = ({ card, transition, index }: AnimatedCardProps) => {
+  // const alpha = transition ? ((index - 1) * Math.PI) / 6 : 0;
+  const style = useAnimatedStyle(() => {
+    const rotate = interpolate(
+      transition.value,
+      [0, 1],
+      [0, ((index - 1) * Math.PI) / 6]
+    );
+    // যদি interpolation সব সময় 0 এবং 1 এর মধ্যে হয় তাহলে mix from reedash use করা যাবে
+    // if it's [0,1] then use mix frmo react-native-redash
+    // const rotate = mix(transition.value, 0, [0, ((index - 1) * Math.PI) / 6]);
+    return {
+      transform: [{ translateX: origin }, { rotate }, { translateX: -origin }],
+    };
+  });
   return (
     <Animated.View key={card} style={[styles.overlay, style]}>
       <Card {...{ card }} />
--- src/Transitions/boilerplate/Transitions.tsx
+++ src/Transitions/boilerplate/Transitions.tsx
@@ -1,5 +1,11 @@
-import React, { useState } from "react";
+import React, { useDebugValue, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
+import {
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
 import { Button, StyleGuide, cards } from "../../components";
 
@@ -13,18 +19,55 @@ const styles = StyleSheet.create({
   },
 });
 
+const useSpring = (state, config) => {
+  const value = useSharedValue(0);
+  useEffect(() => {
+    // eslint-disable-next-line no-nested-ternary
+    value.value = typeof state === "number" ? state : state ? 1 : 0;
+  }, [state, value]);
+
+  return useDerivedValue(() => {
+    return withSpring(value.value, config);
+  });
+};
+
+const useTiming = (state, config) => {
+  const value = useSharedValue(0);
+  useEffect(() => {
+    // eslint-disable-next-line no-nested-ternary
+    value.value = typeof state === "number" ? state : state ? 1 : 0;
+  }, [state, value]);
+
+  return useDerivedValue(() => {
+    return withTiming(value.value, config);
+  });
+};
+
 const UseTransition = () => {
+  // const toggled = useSharedValue(false);
   const [toggled, setToggle] = useState(false);
+
+  const transition = useSpring(toggled);
+  // const transition = useTiming(toggled, { duration: 600 });
+  // const transition = useDerivedValue(() => {
+  //   return withSpring(toggled.value);
+  // });
+
   return (
     <View style={styles.container}>
       {cards.slice(0, 3).map((card, index) => (
-        <AnimatedCard key={card} {...{ index, card, toggled }} />
+        <AnimatedCard key={card} {...{ index, card, transition }} />
       ))}
       <Button
         label={toggled ? "Reset" : "Start"}
         primary
         onPress={() => setToggle((prev) => !prev)}
       />
+      {/* <Button
+        label={toggled ? "Reset" : "Start"}
+        primary
+        onPress={() => (toggled.value = !toggled.value)}
+      /> */}
     </View>
   );
 };

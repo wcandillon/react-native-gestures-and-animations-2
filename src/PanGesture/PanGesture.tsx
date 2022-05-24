23de3633420e39583e5fce74c5b9bcba3600d319
William Candillon <wcandillon@gmail.com>
2022-05-24 22:12:40 +0200

    :lipstick:

--- src/PanGesture/PanGesture.tsx
+++ src/PanGesture/PanGesture.tsx
@@ -7,11 +7,9 @@ import Animated, {
 } from "react-native-reanimated";
 import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
 import { PanGestureHandler } from "react-native-gesture-handler";
-import { clamp } from "react-native-redash";
+import { clamp, withBouncing } from "react-native-redash";
 
 import { Card, Cards, CARD_WIDTH, CARD_HEIGHT } from "../components";
-
-import { withBouncing } from "./withBouncing";
 
 const styles = StyleSheet.create({
   container: {
